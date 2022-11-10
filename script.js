df54f230ec0aa889338269fea14ad81b54f2233f
Mervin Bratic <salj@kwik.se>
2022-11-10 11:20:41 +0100

    changed some game-rules, refactored

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
 
+/* canvas setup */
+
 // animationFrames
-let timer;
+let drawTimer;
+let shuffledTimer;
 let shuffled;
 let animationId;
-const requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-                            window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-
+const requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
 const cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame;
 
 // TODO: observe windowReframe and update new height/width
@@ -28,23 +29,11 @@ canvas.width = width
 canvas.style.width = width
 canvas.style.height = height
 
+/* positions */
+
 const center = {
   x: width / 2,
   y: height / 2,
-}
-
-const gameState = {
-  skillsAvailable: [
-    "css", 
-    "html", 
-    "javascript", 
-    "react", 
-    "node", 
-    "github", 
-    "wordpress", 
-    "stackOverflow", 
-    "google"],
-  skillsEarned: []
 }
 
 // main character position
@@ -64,100 +53,16 @@ const y = function(cent) {
   return per * cent
 }
 
-const linkedInAssets = {
-  small: "/assets/linkedin/linkedin-48.png",
-  medium: "/assets/linkedin/linkedin-96.png",
-  large: "/assets/linkedin/linkedin-144.png",
-  xl: "/assets/linkedin/linkedin-240.png",
-  xxl: "/assets/linkedin/linkedin-480.png",
-}
-
-// get array of images of character
-function getImageFrames(type) {
-  const character = {
-    idle: "/assets/player/idle/",
-    walkingRight: "/assets/player/walking-right/",
-    walkingLeft: "/assets/player/walking-left/",
-    walkingUp: "/assets/player/walking-up/",
-    walkingDown: "/assets/player/walking-down/",
-  }
-
-  const images = []
-  let currentFrame = 0
-  let path;
-
-  while (currentFrame < 8) {
-    path = character[type] + currentFrame + ".gif"
-    images.push(path)
-    currentFrame++
-  }
-
-  return images
-}
-
-function rand() {
-  return Math.ceil(Math.random() * 80) + 5
-}
-
-function renderCharacter(asset, moveX = 0, moveY = 0) {
-  let frameRate = 0
-
-  function draw() {
-    let image = new Image()
-    image.src = getImageFrames(asset)[frameRate]
-    image.onload = function() {
-      // clear
-      ctx.clearRect (0, 0, width, height);
-
-      // if all skills are earned, game over
-      if (gameState.skillsAvailable.length === 0) {
-        renderWinner()
-        return
+function isCloseEnough(skill) {
+  if (characterPosition.x - skillAssets[skill].x > -20 &&
+      characterPosition.x - skillAssets[skill].x < 20 &&
+      characterPosition.y - skillAssets[skill].y > -20 &&
+      characterPosition.y - skillAssets[skill].y < 20) {
+        return true
       }
-      
-      //draw environment
-      renderEnvironment()
-
-      // display gameInfo
-      const displaySkillsAvailable = document.getElementById("available")
-      const displaySkillsEarned = document.getElementById("earned")
-      displaySkillsAvailable.textContent = gameState.skillsAvailable.length
-      displaySkillsEarned.textContent = gameState.skillsEarned.length
-  
-      // for character
-      let offsetWidth = characterPosition.x - (image.width / 2)
-      let offsetHeight = characterPosition.y - (image.height / 2)
-
-      ctx.globalCompositeOperation= 'destination-over';
-      ctx.drawImage(image, offsetWidth, offsetHeight)
-      characterPosition.x += moveX
-      characterPosition.y += moveY
-      timer = setTimeout( () => {
-        animationId = requestAnimationFrame(draw)
-      }, 100)
-    }
-
-    frameRate < 7 ? frameRate += 1 : frameRate = 0
   }
 
-  animationId = requestAnimationFrame(draw)
-}
-
-function renderSkills(asset, positionX, positionY) {
-  let image = new Image()
-  image.src = asset
-  image.onload = function() {
-    let offsetWidth = positionX - (image.width / 2)
-    let offsetHeight = positionY - (image.height / 2)
-    ctx.drawImage(image, offsetWidth, offsetHeight)
-  }
-}
-
-function renderWinner() {
-  ctx.font = "24px Arial";
-  ctx.textAlign = "center"
-  ctx.fillText("You are a frontend-developer!", center.x, center.y);
-}
+/* data */
 
 const skillAssets = {
   images: {
@@ -182,6 +87,128 @@ const skillAssets = {
   stackOverflow: { x, y },
 }
 
+const gameState = {
+  skillsAvailable: [
+    "css", 
+    "html", 
+    "javascript", 
+    "react", 
+    "node", 
+    "github", 
+    "wordpress", 
+    "stackOverflow", 
+    "google"],
+  skillsEarned: []
+}
+
+// get array of images of character
+const getImageFrames = function(type) {
+  const character = {
+    idle: "/assets/player/idle/",
+    walkingRight: "/assets/player/walking-right/",
+    walkingLeft: "/assets/player/walking-left/",
+    walkingUp: "/assets/player/walking-up/",
+    walkingDown: "/assets/player/walking-down/",
+  }
+
+  const images = []
+  let currentFrame = 0
+  let path;
+
+  while (currentFrame < 8) {
+    path = character[type] + currentFrame + ".gif"
+    images.push(path)
+    currentFrame++
+  }
+
+  return images
+}
+
+/* eventDelegation */
+
+function eventDelegation() {
+  const arrows = ["ArrowDown", "ArrowUp", "ArrowLeft", "ArrowRight"]
+  isThrottled = false
+
+  document.addEventListener("keydown", e => {
+    if (e.code === "Space") {
+      shuffled = false
+      clearTimeout(shuffledTimer)
+    }
+  })
+
+  document.addEventListener("keydown", e => {
+    if (isThrottled) return
+    if (!arrows.includes(e.key)) return
+    isThrottled = true
+
+    if (e.key === "ArrowRight") renderCharacter("walkingRight", 5, 0)
+    if (e.key === "ArrowLeft") renderCharacter("walkingLeft", -5, 0,)
+    if (e.key === "ArrowUp") renderCharacter("walkingUp", 0, -5)
+    if (e.key === "ArrowDown") renderCharacter("walkingDown", 0, 5)
+  })
+
+  document.addEventListener("keyup", e => {
+    if (!arrows.includes(e.key)) return
+    isThrottled = false
+    renderCharacter("idle")
+  })
+}
+
+/* view */
+
+function renderCharacter(asset, moveX = 0, moveY = 0) {
+  let frameRate = 0
+
+  function draw() {
+    clearTimeout(drawTimer)
+    cancelAnimationFrame(animationId)
+
+    // prepare current image-frame of character
+    let image = new Image()
+    image.src = getImageFrames(asset)[frameRate]
+    frameRate < 7 ? frameRate += 1 : frameRate = 0
+
+    image.onload = function() {
+      ctx.clearRect (0, 0, width, height);
+
+      // if all skills are earned, game over
+      if (!gameState.skillsAvailable.length) {
+        renderWinner()
+        return
+      }
+      
+      //draw environment
+      renderEnvironment()
+      displayGameStatus()
+
+      // draw character
+      ctx.globalCompositeOperation= 'destination-over';
+      let offsetWidth = characterPosition.x - (image.width / 2)
+      let offsetHeight = characterPosition.y - (image.height / 2)
+      ctx.drawImage(image, offsetWidth, offsetHeight)
+      characterPosition.x += moveX
+      characterPosition.y += moveY
+
+      drawTimer = setTimeout( () => {
+      animationId = requestAnimationFrame(draw)
+      }, 60)
+    }
+  }
+
+  animationId = requestAnimationFrame(draw)
+}
+
+function renderSkills(asset, positionX, positionY) {
+  let image = new Image()
+  image.src = asset
+  image.onload = function() {
+    let offsetWidth = positionX - (image.width / 2)
+    let offsetHeight = positionY - (image.height / 2)
+    ctx.drawImage(image, offsetWidth, offsetHeight)
+  }
+}
+
 function renderEnvironment() {
   const remainingSkills = [...gameState.skillsAvailable]
   const _ = skillAssets
@@ -193,49 +220,36 @@ function renderEnvironment() {
 
   if (!shuffled) {
     remainingSkills.forEach(skill => {
-      // if character found the skill, remove from showing up
-      if (characterPosition.x - _[skill].x > -20 &&
-          characterPosition.x - _[skill].x < 20 &&
-          characterPosition.y - _[skill].y > -20 &&
-          characterPosition.y - _[skill].y < 20) {
-            gameState.skillsEarned.push(skill)
-            gameState.skillsAvailable = gameState.skillsAvailable.filter(available => available !== skill)
-          }
+      if (isCloseEnough(skill)) {
+        gameState.skillsEarned.push(skill)
+        gameState.skillsAvailable = gameState.skillsAvailable.filter(available => available !== skill)
+      }
 
       _[skill].x = x(rand()), _[skill].y = x(rand())
-      renderSkills(_.images.css, _.css.x, _.css.y)
     })
   }
 
   shuffled = true
-  setTimeout( () => shuffled = false, 10000)
+  shuffledTimer = setTimeout( () => shuffled = false, 5000)
 }
 
-function eventDelegation() {
-  const arrows = ["ArrowDown", "ArrowUp", "ArrowLeft", "ArrowRight"]
-  isThrottled = false
-
-  document.addEventListener("keydown", e => {
-    if (isThrottled) return
-    if (!arrows.includes(e.key)) return
-    clearTimeout(timer)
-    cancelAnimationFrame(animationId)
-    isThrottled = true
-
-    if (e.key === "ArrowRight") renderCharacter("walkingRight", 5, 0)
-    if (e.key === "ArrowLeft") renderCharacter("walkingLeft", -5, 0,)
-    if (e.key === "ArrowUp") renderCharacter("walkingUp", 0, -5)
-    if (e.key === "ArrowDown") renderCharacter("walkingDown", 0, 5)
-  })
-
-  document.addEventListener("keyup", e => {
-    if (!arrows.includes(e.key)) return
-    clearTimeout(timer)
-    cancelAnimationFrame(animationId)
-    isThrottled = false
-    renderCharacter("idle")
-  })
+function displayGameStatus() {
+  const displaySkillsAvailable = document.getElementById("available")
+  const displaySkillsEarned = document.getElementById("earned")
+  displaySkillsAvailable.textContent = gameState.skillsAvailable.length
+  displaySkillsEarned.textContent = gameState.skillsEarned.length
 }
 
+function renderWinner() {
+  ctx.font = "24px Arial";
+  ctx.textAlign = "center"
+  ctx.fillText("You are a frontend-developer!", center.x, center.y);
+}
+
+function rand() {
+  return Math.ceil(Math.random() * 40) + 15
+}
+
+//initial execution
 renderCharacter("idle")
 eventDelegation()
